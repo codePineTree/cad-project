75bac4d236913b827cf30fb766ba49227122bdd3
Kimjs <kjs@iteyes.co.kr>
2025-10-01 17:18:04 +0900

    Test Mapper 제거

--- src/main/java/com/cad/cadproject/CadProjectApplication.java
+++ src/main/java/com/cad/cadproject/CadProjectApplication.java
@@ -1,6 +1,6 @@
 package com.cad.cadproject;
 
-import com.cad.cadproject.mapper.TestMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,8 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadProjectApplication implements CommandLineRunner {
 
-    @Autowired
-    private TestMapper testMapper;
+
 
     public static void main(String[] args) {
         SpringApplication.run(CadProjectApplication.class, args);
@@ -18,7 +17,7 @@ public class CadProjectApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String msg = testMapper.selectTest();
-        System.out.println("MyBatis 테스트 결과: " + msg);
+
+
     }
 }
